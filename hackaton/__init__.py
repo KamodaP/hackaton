51680351fb48029f6546bd8cb0e12252d7bcf023
Sobczak Malgorzata <sobczakmalgorz@gmail.com>
2016-02-20 22:51:36 +0100

    hotfix __init__.py

--- hackaton/__init__.py
+++ hackaton/__init__.py
@@ -25,11 +25,10 @@ def main(global_config, **settings):
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(authz_policy)
     
-    config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('about', '/about')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
-    config.add_static_view(name='static', path='hackaton:static')
+    config.add_static_view(name='static', path='hackaton:static', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
