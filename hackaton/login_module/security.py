60b1c28617d550b4abd853822e25fd9c42838f47
Piotr Kamoda <kamodap@hotmail.com>
2016-02-20 15:50:44 +0100

    Database connection for user authentication

--- hackaton/login_module/security.py
+++ hackaton/login_module/security.py
@@ -1,7 +1,16 @@
-USER = {'editor': 'editor',
+﻿USERS = {'editor': 'editor',
 	'viewer': 'viewer'}
 GROUPS = {'editor': ['group:editors']}
+
+from ..database.model import DBSession, users
 
 def groupfinder(userid, request):
 	if userid in USERS:
 		return GROUPS.get(userid, [])
+
+def check_login(userid, passwd):
+    user = BDSession.query(users).filter_by(email_addr = userid, pswd_hash = passwd).first()
+    if user != None:
+        return True
+    else:
+        return False
