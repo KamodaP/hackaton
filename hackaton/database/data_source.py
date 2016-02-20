3d0872475967c7370ba2b8702a389027a868126d
Sobczak Malgorzata <sobczakmalgorz@gmail.com>
2016-02-20 18:27:27 +0100

    basic data access

---
+++ hackaton/database/data_source.py
@@ -0,0 +1,10 @@
+from .models import DBSession, data, games,users
+
+def get_game_data(gameid):
+    game_data = DBSession.query(data).filter_by(game_id == gameid).order_by(data.id).all()
+    return game_data
+
+def set_data(gameid, val1, val2):
+    dataset = data(game_id = gameid, value_1 = val1, value_2 = val2)
+    DBSession.add(dataset)
+    return True
