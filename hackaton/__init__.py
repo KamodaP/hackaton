c8f81049ed9d547aa77dbc6b8e217c0992cadc27
Sobczak Malgorzata <sobczakmalgorz@gmail.com>
2016-02-20 16:08:22 +0100

    merge __init__.py

--- hackaton/__init__.py
+++ hackaton/__init__.py
@@ -1,5 +1,9 @@
-﻿from pyramid.config import Configurator
+﻿from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+
+from .security import groupfinder
 
 from scripts.models import (
     DBSession,
@@ -15,7 +19,18 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+	
+	#Policies
+	authn_policy = AuthTktAuthenticationPolicy(settings['hackaton.secret'], 
+		callback = groupfinder, hashalg='sha512')
+	authz_policy = ACLAuthorizationPolicy()
+	config.set_authentication_policy(authn_policy)
+	config.set_authorization_policy(authz_policy)
+	
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+	config.add_route('about', '/about')
+	config.add_route('login', '/login')
+	config.add_route('logout', '/logout')
     config.scan()
     return config.make_wsgi_app()
