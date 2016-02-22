afd8818dfa8ef08a32d3c59267218c95bcacfb0a
Piotr Kamoda <kamodap@hotmail.com>
2016-02-22 02:47:50 +0100

    hf

--- hackaton/__init__.py
+++ hackaton/__init__.py
@@ -35,6 +35,7 @@ def main(global_config, **settings):
     config.add_route('user_games', '/user_games')
     config.add_route('game_view', '/game_view')
     config.add_route('flashcard', '/flashcard')
+    config.add_route('next_flash', '/next_flash')
     config.add_static_view(name='static', path='hackaton:static', cache_max_age=3600)
     config.scan()
     return config.make_wsgi_app()
